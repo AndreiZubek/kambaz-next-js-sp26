d9c5641038bd4acc90f2ccf1c7c52fac7885b9b3
Andrei <zubek.a@northeastern.edu>
2026-03-15 20:41:03 -0400

    fix deployment

--- app/labs/lab4/redux/page.tsx
+++ app/labs/lab4/redux/page.tsx
@@ -1,15 +1,19 @@
 "use client";
+import { Provider } from "react-redux";
 import AddRedux from "./AddRedux";
 import CounterRedux from "./CounterRedux";
 import HelloRedux from "./hello";
+import store from "../store";
 
 export default function ReduxExamples() {
   return (
-    <div>
-      <h2>Redux Examples</h2>
-      <HelloRedux />
-      <CounterRedux />
-      <AddRedux />
-    </div>
+    <Provider store={store}>
+      <div>
+        <h2>Redux Examples</h2>
+        <HelloRedux />
+        <CounterRedux />
+        <AddRedux />
+      </div>
+    </Provider>
   );
 }
